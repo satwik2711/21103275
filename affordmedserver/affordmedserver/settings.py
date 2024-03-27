f61819c193b5cb1336d9cd74d21460966e45b524
satwik2711 <satwik3.14159@gmail.com>
2024-03-27 14:42:22 +0530

    test

--- affordmedserver/affordmedserver/settings.py
+++ affordmedserver/affordmedserver/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'rest_framework',
+    'api',
 ]
 
 ROOT_URLCONF = 'affordmedserver.urls'
