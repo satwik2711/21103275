60632cc30f75c78d5d34698918bc103f48cb6db0
satwik2711 <satwik3.14159@gmail.com>
2024-03-27 16:58:46 +0530

    final changes

--- affordmedserver/api/views.py
+++ affordmedserver/api/views.py
@@ -12,16 +12,27 @@ from django.conf import settings
 
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def product_list(request, categoryname):
-    sort_by = request.query_params.get('sort', 'rating')
+    sort_by = request.query_params.get('sort', None)
     page = int(request.query_params.get('page', 1))
     n = int(request.query_params.get('n', 10))
+    min_price = request.query_params.get('minPrice', None)
+    max_price = request.query_params.get('maxPrice', None)
 
-    products = fetch_products(categoryname, sort_by)
-    sorted_products = sort_products(products, sort_by)
+    companies = AccessToken.objects.all()
+    all_products = []
+    for company in companies:
+        access_token = company.access_token
+        company_products = fetch_products(access_token, company.company_name, categoryname, n, min_price, max_price)
+        all_products.extend(company_products)
+
+    sorted_products = sort_products(all_products, sort_by)
     page_of_products = paginate_products(sorted_products, page, n)
 
     return Response(page_of_products)
+
+
 
 
 
